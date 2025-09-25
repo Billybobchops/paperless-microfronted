c26b0b10c5741c0620c3ec11612e5d0c659e5a51
Billybobchops <chlanda52@gmail.com>
2025-09-25 09:32:01 -0400

    test eager true

--- module-federation.config.ts
+++ module-federation.config.ts
@@ -11,9 +11,9 @@ export default createModuleFederationConfig({
     },
     filename: 'remoteEntry.js',
     shared: {
-        react: { singleton: true, requiredVersion: '^18.0.0' },
-        'react-dom': { singleton: true, requiredVersion: '^18.0.0' },
-        'react-router-dom': { singleton: true, requiredVersion: '^6.0.0' },
+        react: { singleton: true, requiredVersion: '^18.0.0', eager: true },
+        'react-dom': { singleton: true, requiredVersion: '^18.0.0', eager: true },
+        'react-router-dom': { singleton: true, requiredVersion: '^6.0.0', eager: true },
         clsx: { singleton: true, requiredVersion: '^2.0.0' },
         'react-hook-form': { singleton: true, requiredVersion: '^7.0.0' },
     },
